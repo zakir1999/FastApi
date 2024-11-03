8c0041c1350932132641d506026dda349baf8b42
zakir1999 <m.zakirhossen99@gmail.com>
2024-11-04 00:35:02 +0600

    CRUD Operation

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 # all packages
 from fastapi import FastAPI
-
-
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 
 # Main function
 app=FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str 
+    published: bool=True
+    rating:Optional[int]=None
 
+my_posts=[{"title":"title of post 1","content":"content of post 1","id":1},{"title":"Favorite foods","content":"I like pizza","id":2}]
 @app.get("/")
 def root():
     return {"message":"Hello I am zakir"}
@@ -15,4 +22,13 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"This is your posts"}
+    return {"data":my_posts}
+
+
+@app.post("/posts")
+def create_posts(post:Post):
+    print(post.rating)
+    return {"data":post}
+
+
+# Title str, Content str
